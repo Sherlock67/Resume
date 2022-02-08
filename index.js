118ce6f00155422ed59c9d532df08947d7aeb2a4
Sherlock67 <alammushfiqul366@gmail.com>
2022-02-08 18:26:46 +0600

    need smoothing

---
+++ index.js
@@ -0,0 +1,8 @@
+function show(){
+    document.getElementById('edu').style.display = 'block';
+    document.getElementById('skill').style.display = 'none';
+}
+function show1(){
+    document.getElementById('edu').style.display = 'none';
+    document.getElementById('skill').style.display = 'block';
+}
